22fc89478e495afeb6f9e574c5d586c6ee0f4ee3
irtiza naqvi <naqviirtiza9@gmail.com>
2026-01-28 16:52:26 +0500

    User and hospital initialized

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { HeaderComponent } from './app/header.component';
+import { HeaderComponent } from './app/header/header.component';
+import { UserComponent } from './app/user/user.component';
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
 bootstrapApplication(HeaderComponent).catch((err)=> console.error(err));
+bootstrapApplication(UserComponent).catch((err)=> console.error(err))
