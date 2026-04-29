83972f5ad7bdbd2eaf4e970b6389961b69e31824
irtiza naqvi <naqviirtiza9@gmail.com>
2026-04-29 13:32:32 +0500

    Login form update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from "./auth/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
