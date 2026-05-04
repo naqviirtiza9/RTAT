5b0780704797226ae3f4fc4f9c1881c0d88b3e55
irtiza naqvi <naqviirtiza9@gmail.com>
2026-05-04 16:52:54 +0500

    Folder Structure Changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { LoginComponent } from "./auth/login/login.component";
+import { LoginComponent } from "./auth/User/login/login.component";
 
 @Component({
   selector: 'app-root',
